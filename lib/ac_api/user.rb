b8cd8f16898382ee8281821d8b5a72c77fbae7fc
Diogo Soares <diogo@jera.com.br>
2012-11-15 17:03:37 -0300

    Inserindo usuario

---
+++ lib/ac_api/user.rb
@@ -0,0 +1,8 @@
+class AcApi::User
+
+  def user_info(api_url, token)
+    url = "#{api_url}?auth_api_token=#{token}&path_info=info"
+    response = HTTParty.get(url)
+    response["info"].first[1]
+  end
+end
